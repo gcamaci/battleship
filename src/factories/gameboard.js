20733c25037381ae0b601ed2780eda743b92888d
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-02-15 08:23:30 -0600

    adding comments to give me guidance for new functions

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -3,11 +3,12 @@ const GameBoard = () => {
     const ships = [];
     const attempts = []
     const getShips = () => ships;
-
+//place ship object which contains the battleship as a property and an array of the coordinates.
     const placeShip = (ship,...coords) => {
         let cors = coords
         ships.push({ship,cors});
     }
+
 
     const receiveAttack = (cord) => {
         ships.forEach((shipObj) => {
@@ -18,6 +19,8 @@ const GameBoard = () => {
         attempts.push(cord)
     }
 
+    //create pure function to push coordinate to attempts. 
+    //create pure function to remove cord from ships coordinate array. I think this could be in ship factory itself?
     
 
     return{placeShip,getShips,ships,receiveAttack,attempts}
