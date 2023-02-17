1564470c34881517ddc30f2841c40a73a52d4435
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-02-17 12:58:56 -0600

    player object built, attack function with tests completed. Player can attack enemy game board

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,5 +1,10 @@
-const Player = (enemyGameBoard) => {
+const Player = (gameBoard) => {
 
+
+    const attack = (cord,enemyBoard) => {
+        enemyBoard.receiveAttack(cord)
+    }
+    return {attack}
 }
 
 export {Player}
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,12 +1,23 @@
+import { GameBoard } from "../factories/gameboard";
 import { Player } from "../factories/player";
+import { Ship } from "../factories/ship";
 
 test('player can attack enemy board', () => {
-    //mock function of a gameBoard.
-    //pretend we dont have gameBoard needed
-    const gameBoard = {
-        placeShip: jest.fn(),
-        receiveAttack: jest.fn(),
-        getShips: jest.fn(()=>[]),
-        attempts: []
-    }
+    const battleShip = Ship(3)
+    const enemyGameBoard = GameBoard()
+    enemyGameBoard.placeShip(battleShip,30,31,32)
+    expect(enemyGameBoard.getShips()).toHaveLength(1)
+
+    const playerBoard = GameBoard()
+    const player = Player(playerBoard)
+    player.attack(52,enemyGameBoard)
+    expect(enemyGameBoard.getShips()[0].ship.getHealth()).toBe(3)
+    player.attack(30,enemyGameBoard)
+    expect(enemyGameBoard.getShips()[0].ship.getHealth()).toBe(2)
+    player.attack(31,enemyGameBoard)
+    player.attack(32,enemyGameBoard)
+    expect(enemyGameBoard.getShips()[0].ship.isSunk()).toBeTruthy()
+    
+
+
 });
